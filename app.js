76f1592e82f17ef88084f81579f63fb3356c31bf
bhavyakaria <bhavyakaria@yahoo.in>
2020-06-23 23:48:29 +0530

    sample backend setup

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+require('dotenv').config();
+const Twitter = require('twitter');
+const config = require('./config/config.js');
+
+const app = express();
+const twitter = new Twitter(config);
+
+// Set up your search parameters
+const params = {
+    q: 'Reliance Industries',
+    count: 10,
+    result_type: 'popular',
+    lang: 'en'
+}
+
+twitter.get('search/tweets', params, function (err, data, response) {
+    if (!err) {
+        const tweets = data['statuses'];
+        for (let i = 0; i < tweets.length; i++) {
+            console.log(tweets[i]['text']);
+        }
+    } else {
+        console.log(err);
+    }
+});
+
+// Bodyparser
+app.use(express.urlencoded({ extended: false }));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server started on ${PORT}`));
